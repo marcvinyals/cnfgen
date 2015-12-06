abb9c52ed51f4ca44afb4e9c9cc218144492ed23
Marc Vinyals <vinyals@kth.se>
2015-12-06 23:59:11 +0100

    Add a class that talks opb

---
+++ cnfformula/opb.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+class OPB(object):
+    def __init__(self):
+        self._index2name = []
+        self._name2index = {}
+        self._constraints = []
+
+    def to_opb(self):
+        from cStringIO import StringIO
+        output = StringIO()
+        for c in self._constraints:
+            for term in c[:-1]:
+                coefficient = term[0]
+                literal = "x" + str(self._name2index[term[2]])
+                if not term[1] :
+                    literal = "~" + literal
+                output.write("{} {} ".format(coefficient,literal))
+            output.write(">= {} ;\n".format(c[-1]))
+
+        return output.getvalue()
+        
+    def add_variable(self, var):
+        self._index2name.append(var)
+        self._name2index[var] = len(self._index2name)-1
+
+    def add_constraint(self, constraint):
+        self._constraints.append(constraint)
+        
+    def add_clause(self, clause):
+        self.add_constraint([(1,)+lit for lit in clause] + [1])
+
+    def add_geq_constraint(self, variables, lower_bound):
+        self.add_constraint([(1,True,var) for var in variables] + [lower_bound])
+        
+    def add_leq_constraint(self, variables, upper_bound):
+        self.add_constraint([(-1,True,var) for var in variables] + [upper_bound])
+
+    def add_eq_constraint(self, variables, value):
+        self.add_geq_constraint(variables, value)
+        self.add_leq_constraint(variables, value)
