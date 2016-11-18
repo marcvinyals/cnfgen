e4bb163e979298381c68538474089f42a31dc621
Marc Vinyals <vinyals@kth.se>
2016-11-18 21:36:49 +0000

    OPB version of dominating set

--- cnfformula/families/dominatingset.py
+++ cnfformula/families/dominatingset.py
@@ -4,7 +4,7 @@
 """
 
 
-from cnfformula.cnf import CNF
+from cnfformula.opb import OPB
 from cnfformula.cmdline import SimpleGraphHelper
 
 from cnfformula.cmdline  import register_cnfgen_subcommand
@@ -13,9 +13,38 @@ from cnfformula.families import register_cnf_generator
 from cnfformula.graphs import enumerate_vertices,enumerate_edges,neighbors
 from itertools import combinations,combinations_with_replacement,product
 
+@register_cnf_generator
+def DominatingSet(G):
+    F=OPB()
+
+    def D(v):
+        return "x_{{{0}}}".format(v)
+
+    def N(v):
+        return tuple(sorted([ v ] + [ u for u in G.neighbors(v) ]))
+
+    # Fix the vertex order
+    V=enumerate_vertices(G)
+
+    avgdegree=sum(len(set(N(v))) for v in V)/len(V)
+    d=len(V)/avgdegree
+    
+    # Create variables
+    for v in V:
+        F.add_variable(D(v))
+
+    # Not too many true variables
+    F.add_leq_constraint([D(v) for v in V],d)
+
+    # Every neighborhood must have a true D variable
+    neighborhoods = sorted( set(N(v) for v in V) )
+    for N in neighborhoods:
+        F.add_clause([ (True,D(v)) for v in N])
+        
+    return F
 
 @register_cnf_generator
-def DominatingSet(G,d, alternative = False):
+def MappingDominatingSet(G,d, alternative = False):
     r"""Generates the clauses for a dominating set for G of size <= d
 
     The formula encodes the fact that the graph :math:`G` has
@@ -123,8 +152,6 @@ class DominatingSetCmdHelper(object):
         Arguments:
         - `parser`: parser to load with options.
         """
-        parser.add_argument('d',metavar='<d>',type=int,action='store',help="size of the dominating set")
-        parser.add_argument('--alternative','-a',action='store_true',default=False,help="produce the provably hard version")
         SimpleGraphHelper.setup_command_line(parser)
 
 
@@ -136,7 +163,4 @@ class DominatingSetCmdHelper(object):
         - `args`: command line options
         """
         G = SimpleGraphHelper.obtain_graph(args)
-        return DominatingSet(G, args.d, alternative = args.alternative )
-
-
-
+        return DominatingSet(G)
