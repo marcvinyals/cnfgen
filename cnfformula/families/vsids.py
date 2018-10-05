4a4af8125b17e587aa1a37258a9827af8cd3da6c
Marc Vinyals <marc.vinyals@tifr.res.in>
2018-10-05 19:13:30 +0530

    Alternate versions of vsids formula

--- cnfformula/families/vsids.py
+++ cnfformula/families/vsids.py
@@ -8,6 +8,7 @@ import cnfformula.families
 
 from cnfformula.graphs import neighbors
 from itertools import product
+import random
 
 @cnfformula.families.register_cnf_generator
 def VsidsFormula(n):
@@ -29,43 +30,63 @@ def VsidsFormula(n):
         functional = False,
         surjective = False)
 
-    m=n
+    m=n*n
     k=4
+    xx = [php_var_name(p,h) for p in range(1,pigeons+1) for h in range(1,holes+1)]
     xx1 = [php_var_name(p,h) for p in range(1,pigeons+1) for h in range(1,holes//2+1)]
     xx2 = [php_var_name(p,h) for p in range(1,pigeons+1) for h in range(holes//2+1,holes+1)]
     yy1 = ["y"+str(i) for i in range(1,m+1)]
     yy2 = ["yy"+str(i) for i in range(1,m+1)]
     zz = ["z"+str(i) for i in range(1,k+1)]
-    
-    for y in yy1:
-        vsids.add_variable(y)
-    
-    for v in mapping.variables():
-        vsids.add_variable(v)
 
-    for z in zz:
-        vsids.add_variable(z)
+    def add_variables12xz21():
+        for y in yy1[0:len(yy1)//2]:
+            vsids.add_variable(y)
+        for y in yy2[0:len(yy2)//2]:
+            vsids.add_variable(y)
+        for x in xx:
+            vsids.add_variable(x)
+        for z in zz:
+            vsids.add_variable(z)
+        for y in yy2[len(yy2)//2:]:
+            vsids.add_variable(y)
+        for y in yy1[len(yy1)//2:]:
+            vsids.add_variable(y)
+    add_variables12xz21()
 
-    for y in yy2:
-        vsids.add_variable(y)
-        
     for c in mapping.clauses():
         vsids.add_clause_unsafe(c)
-        
-    for y in yy1:
+
+    def add_pitfall_seq(y,S):
         c=[(True,y)]
-        for x in xx1:
-            vsids.add_clause(c+[(False,x)])
-            c.append((True,x))
-        vsids.add_clause(c)
-    for y in yy2:
-        c=[(True,y)]
-        for x in xx2:
+        for x in S:
             vsids.add_clause(c+[(False,x)])
             c.append((True,x))
         vsids.add_clause(c)
 
-    for polarity in product((True,False),repeat=4):
+    def add_pitfall_par(y,S):
+        c=[(True,y)]
+        for x in S:
+            vsids.add_clause([(True,y)]+[(False,x)])
+            c.append((True,x))
+        vsids.add_clause(c)
+
+    def add_pitfall(y,S):
+        add_pitfall_seq(y,S)
+
+    def add_clauses1212():
+        for y in yy1:
+            add_pitfall(y,xx1)
+        for y in yy2:
+            add_pitfall(y,xx2)
+
+    def add_clauses23rand():
+        for y in yy1+yy2:
+            add_pitfall(y,random.sample(xx,2*len(xx)//3))
+
+    add_clauses23rand()
+
+    for polarity in product((True,False),repeat=k):
         vsids.add_clause(zip(polarity,zz))
 
     return vsids
