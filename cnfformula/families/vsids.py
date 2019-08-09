5ba4859845b3b0e3dadfbae01e5c7e1e8523d8f3
Marc Vinyals <vinyals@kth.se>
2019-08-09 10:53:11 +0200

    Tseitin version of vsids formula

--- cnfformula/families/vsids.py
+++ cnfformula/families/vsids.py
@@ -103,3 +103,113 @@ class VsidsCmdHelper(object):
     @staticmethod
     def build_cnf(args):
         return VsidsFormula(args.n)
+
+import networkx
+from cnfformula.families.tseitin import TseitinFormula
+from itertools import combinations
+
+@cnfformula.families.register_cnf_generator
+def VsidsFormulaTs(n,d,m,l,k):
+    vsids=CNF()
+    graph=networkx.random_regular_graph(d,n)
+    charge=[1]+[0]*(n-1)
+    ts = TseitinFormula(graph,charge)
+
+    X = list(ts.variables())
+    for x in X:
+        vsids.add_variable(x)
+
+    def yname(j,i):
+        return "y_{}_{}".format(j,i)
+
+    def zname(j,i):
+        return "z_{}_{}".format(j,i)
+
+    def tname(y,z,i):
+        return "t_{}_{}_{}".format(y,z,i)
+
+    Y = [0]*k
+    Z = [0]*k
+    for j in range(k):
+        Y[j] = [yname(j,i) for i in range(m)]
+        Z[j] = [zname(j,i) for i in range(l)]
+
+    # Ts_j
+    for j in range(k):
+        append = [(True,z) for z in Z[j]]
+        for C in ts:
+            vsids.add_clause(C + append)
+
+    # Psi
+    def pitfall1(y,S):
+        C = [(True,y)]
+        for s in S:
+            vsids.add_clause(C+[(False,s)])
+            C.append((True,s))
+
+    for j in range(k):
+        for y in range(m):
+            #pitfall1(yname(j,y),X+Z[j])
+            pass
+
+    def pitfall2(y1,y2,S):
+        C = [(True,y1),(True,y2)]
+        for s in S:
+            vsids.add_clause(C+[(False,s)])
+            C.append((True,s))
+
+    for j in range(k):
+        for (y1,y2) in combinations(range(m),2):
+            pitfall2(yname(j,y1),yname(j,y2),X+Z[j])
+            #pass
+
+    # Delta
+    def tail1(z):
+        vsids.add_clause([(True,tname('',z,1)),(False,z)])
+        vsids.add_clause([(False,tname('',z,1)),(False,z)])
+
+    for j in range(k):
+        for z in Z[j]:
+            #tail1(z)
+            pass
+
+    def tail2(y,z):
+        vsids.add_clause([(True,tname(y,z,1)),(True,tname(y,z,3)),(False,y)])
+        vsids.add_clause([(True,tname(y,z,2)),(False,tname(y,z,3)),(False,y)])
+        vsids.add_clause([(False,tname(y,z,1)),(False,z),(False,y)])
+        vsids.add_clause([(False,tname(y,z,2)),(False,z),(False,y)])
+
+    for j in range(k):
+        for (y,z) in product(Y[j],Z[j]):
+            tail2(y,z)
+            #pass
+
+    # Gamma
+    vsids.add_clause([(False,yname(j,i)) for i in range(m) for j in range(k)])
+
+    for i in range(m):
+        vsids.add_clause([(False,yname(j,i)) for j in range(k)])
+
+    for i in range(0,m,2):
+        #vsids.add_clause([(False,yname(j,i)) for j in range(k)] +
+        #                 [(False,yname(j,i+1)) for j in range(k)])
+        pass
+
+    return vsids
+
+@cnfformula.cmdline.register_cnfgen_subcommand
+class VsidsCmdHelper(object):
+    name='vsidsts'
+    description='hard VSIDS formula'
+
+    @staticmethod
+    def setup_command_line(parser):
+        parser.add_argument('n',metavar='<n>',type=int,help="n")
+        parser.add_argument('d',metavar='<d>',type=int,help="d")
+        parser.add_argument('m',metavar='<m>',type=int,help="m")
+        parser.add_argument('l',metavar='<l>',type=int,help="l")
+        parser.add_argument('k',metavar='<k>',type=int,help="k")
+
+    @staticmethod
+    def build_cnf(args):
+        return VsidsFormulaTs(args.n, args.d, args.m, args.l, args.k)
